d1e0a534fc1f1c242f393d306afa0cf696d6390a
Gianluca755 <ermacora.gianluca25@gmail.com>
2021-06-03 15:33:03 +0200

    fix vue-router for navigating to the same location

--- src/router/index.js
+++ src/router/index.js
@@ -28,3 +28,13 @@ export default function (/* { store, ssrContext } */) {
 
   return Router
 }
+
+// Modify the push method of vue-router
+
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject) {
+    return originalPush.call(this, location, onResolve, onReject)
+  }
+  return originalPush.call(this, location).catch(err => err)
+}
